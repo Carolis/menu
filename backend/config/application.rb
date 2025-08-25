b70c0cd801ea2c1f818fa781cad24fb1f5a8eaba
carolis <redrumjedi@gmail.com>
2025-08-25 19:16:39 -0300

    feat: update gems, initialize rspec and add basic cors config

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Backend
   class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "http://localhost:5173" # Vite default port
+        resource "*", headers: :any, methods: [ :get, :post, :patch, :put, :delete, :options ]
+      end
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
