79a722c9a4baaa9e8b6dbb1f8a8ffbbcf929ea9b
carolis <redrumjedi@gmail.com>
2025-08-25 21:20:14 -0300

    feat: add Menu and MenuItem models and updates the conflicts on schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,44 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_01_002428) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_26_001146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "likes", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "photo_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["photo_id"], name: "index_likes_on_photo_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
-  create_table "photos", force: :cascade do |t|
-    t.string "image_url"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "pexels_id", null: false
-    t.integer "width"
-    t.integer "height"
-    t.string "photographer"
-    t.string "photographer_url"
-    t.string "avg_color"
-    t.text "alt_text"
-    t.index ["pexels_id"], name: "index_photos_on_pexels_id", unique: true
-    t.index ["user_id"], name: "index_photos_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
+  create_table "menu_items", force: :cascade do |t|
     t.string "name"
-    t.string "email"
-    t.string "password_digest"
+    t.decimal "price"
+    t.bigint "menu_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["menu_id"], name: "index_menu_items_on_menu_id"
+  end
+
+  create_table "menus", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "likes", "photos"
-  add_foreign_key "likes", "users"
-  add_foreign_key "photos", "users"
+  add_foreign_key "menu_items", "menus"
 end
